21527a9a201b90a3d4eefeaecbca35046769f069
KrupaliAkbari6 <krupali060802@gmail.com>
2024-07-16 21:11:14 +0530

    layout

---
+++ ContraintLayout/app/src/main/java/com/example/contraintlayout/MainActivity.kt
@@ -0,0 +1,29 @@
+package com.example.contraintlayout
+
+import android.os.Bundle
+import android.widget.TextView
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        var textview:TextView=findViewById(R.id.textView2)
+
+        textview.setOnClickListener()
+        {
+            Toast.makeText(applicationContext,"Hello MCA",Toast.LENGTH_LONG).show()
+        }
+    }
+}
